abf35da0ab20eed53799157451853fb2fdb15ddc
kuanghua <869653722@qq.com>
2021-11-26 19:56:06 +0800

    fix some issue and add func

---
+++ src/views/permission/directive.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <el-button v-permission="['admin']">showing when then role of admin</el-button>
+    <el-button v-permission="['editor']">editor</el-button>
+    <el-button v-permission="['admin', 'editor']">editor and admin</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
