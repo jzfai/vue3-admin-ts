5d48dc452c619b703f4124774a06992057033e20
kuanghua <869653722@qq.com>
2021-12-19 17:49:41 +0800

    fix ts fix error

---
+++ src/types/mixin.d.ts
@@ -0,0 +1,9 @@
+/* commonMixin.js声明文件,如果使用到了mixin里的方法请在此处进行配置声明*/
+// data
+declare var fileListMixin: Array<any>
+declare var chooseFileNameMixin: string
+declare var commonValueMixin: string
+declare var pageTotalMixin: number
+declare var dialogTitleMixin: string
+declare var detailDialogMixin: boolean
+declare var startEndArrMixin: Array<any>
