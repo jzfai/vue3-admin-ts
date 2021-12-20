88fbf87418a04c5dc41fe0a5af43b2b03a601616
jzfai <869653722@qq.com>
2021-12-20 15:02:16 +0800

    chang yarn to pnpm

---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+//  the ts of vite
+interface ImportMetaEnv {
+  readonly VITE_APP_BASE_URL: string
+  readonly VITE_APP_IMAGE_URL: string
+  readonly VITE_APP_ENV: string
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,11 +1,23 @@
-/*生命模块类型*/
+/*the ts file of vue*/
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+//declare import module
 declare module './*'
 declare module '@/*'
 declare module '@/hooks'
 declare module '*'
+
+/* commonMixin.js声明文件,如果使用到了mixin里的方法请在此处进行配置声明*/
+declare var fileListMixin: Array<any>
+declare var chooseFileNameMixin: string
+declare var commonValueMixin: string
+declare var pageTotalMixin: number
+declare var dialogTitleMixin: string
+declare var detailDialogMixin: boolean
+declare var startEndArrMixin: Array<any>
+declare var searchFormMixin: any
