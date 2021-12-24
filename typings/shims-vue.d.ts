d39032db37379b623235a5341e6145127792db0f
jzfai <869653722@qq.com>
2021-12-24 12:03:40 +0800

    add Global var demo

---
+++ typings/shims-vue.d.ts
@@ -0,0 +1,13 @@
+/*the ts file of vue*/
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+//declare import module
+declare module './*'
+declare module '@/*'
+declare module '@/hooks'
+declare module '*'
