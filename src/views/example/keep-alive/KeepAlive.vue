f8cad78a29168eb61ab3325fd8e2602665a6073f
jzfai <869653722@qq.com>
2022-08-16 13:05:50 +0800

    look v1.9.1 update logs

--- src/views/example/keep-alive/KeepAlive.vue
+++ src/views/example/keep-alive/KeepAlive.vue
@@ -3,10 +3,10 @@
     <div>KeepAlive.vue</div>
     <el-form ref="refsearchForm" :inline="true" class="mt2">
       <el-form-item label-width="0px" label="" prop="errorLog" label-position="left">
-        <el-input v-model="searchForm.errorLog" class="w150" placeholder="input to test keepAlive" />
+        <el-input v-model="searchForm.errorLog" class="w-150px" placeholder="input to test keepAlive" />
       </el-form-item>
       <el-form-item label-width="0px" label="" prop="pageUrl" label-position="left">
-        <el-input v-model="searchForm.pageUrl" class="w150" placeholder="input to test keepAlive" />
+        <el-input v-model="searchForm.pageUrl" class="w-150px" placeholder="input to test keepAlive" />
       </el-form-item>
     </el-form>
     <el-button type="primary" @click="routerDemoF">to routerDemoF.vue</el-button>
