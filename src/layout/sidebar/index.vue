dc5f3b2b2b09549f2d0a09a90ace81c4d3883b1e
jzfai <869653722@qq.com>
2023-02-06 14:15:48 +0800

    fix some already know issue

--- src/layout/sidebar/index.vue
+++ src/layout/sidebar/index.vue
@@ -25,9 +25,9 @@ import Logo from './Logo.vue'
 import SidebarItem from './SidebarItem.vue'
 import { useBasicStore } from '@/store/basic'
 const { settings, allRoutes, sidebar } = storeToRefs(useBasicStore())
-const route = useRoute()
+const routeInstance = useRoute()
 const activeMenu = computed(() => {
-  const { meta, path } = route
+  const { meta, path } = routeInstance
   // if set path, the sidebar will highlight the path you set
   if (meta.activeMenu) {
     return meta.activeMenu
@@ -40,4 +40,13 @@ const activeMenu = computed(() => {
 .el-menu-vertical {
   width: var(--side-bar-width);
 }
+.reset-menu-style{
+  border-right: 1px solid var(--side-bar-border-right-color);
+}
 </style>
+
+
+<style lang="scss">
+
+</style>
+
