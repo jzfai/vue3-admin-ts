2e6208df4f190836158bd9c43dda96cdec082b61
jzfai <869653722@qq.com>
2023-07-27 15:54:06 +0800

    fix element-plus version issue

--- src/utils/axios-req.ts
+++ src/utils/axios-req.ts
@@ -5,11 +5,27 @@ import { useBasicStore } from '@/store/basic'
 //使用axios.create()创建一个axios请求实例
 const service = axios.create()
 let loadingInstance: any = null //loading实例
-let tempReqUrlSave:any = ''
+let tempReqUrlSave = ''
+let authorTipDoor = true
+
+const noAuthDill = () => {
+  authorTipDoor = false
+  ElMessageBox.confirm('请重新登录', {
+    confirmButtonText: '重新登录',
+    closeOnClickModal: false,
+    showCancelButton: false,
+    showClose: false,
+    type: 'warning'
+  }).then(() => {
+    useBasicStore().resetStateAndToLogin()
+    authorTipDoor = true
+  })
+}
+
 //请求前拦截
 service.interceptors.request.use(
-  (req) => {
-    const { token, axiosPromiseArr } = useBasicStore()
+  (req: any) => {
+    const { token, axiosPromiseArr }: any = useBasicStore()
     //axiosPromiseArr收集请求地址,用于取消请求
     req.cancelToken = new axios.CancelToken((cancel) => {
       tempReqUrlSave = req.url
@@ -18,11 +34,11 @@ service.interceptors.request.use(
         cancel
       })
     })
+
     //设置token到header
-    req.headers['AUTHORIZE_TOKEN'] = token
+    if (token) req.headers['Authorization'] = token
     //如果req.method给get 请求参数设置为 ?name=xxx
-    // @ts-ignore
-    if ('get'.includes(req.method?.toLowerCase())) req.params = req.data
+    if ('get'.includes(req.method?.toLowerCase()) && !req.params) req.params = req.data
 
     //req loading
     // @ts-ignore
@@ -32,10 +48,9 @@ service.interceptors.request.use(
         fullscreen: true,
         // spinner: 'CircleCheck',
         text: '数据载入中...',
-        background: 'rgba(0, 0, 0, 0.3)'
+        background: 'rgba(0, 0, 0, 0.1)'
       })
     }
-
     return req
   },
   (err) => {
@@ -45,46 +60,53 @@ service.interceptors.request.use(
 )
 //请求后拦截
 service.interceptors.response.use(
-  (res) => {
-    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave) //从请求集合中移除
+  (res: any) => {
+    //取消请求
+    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave)
     if (loadingInstance) {
       loadingInstance && loadingInstance.close()
     }
-
     //download file
-    if (['application/zip', 'zip', 'blob', 'arraybuffer'].includes(res.headers['content-type'])) {
-      return res
+    const noAuthCode = '401,403'
+    if (res.data?.size) {
+      new Response(res.data).text().then((stringData) => {
+        const parseJson = JSON.parse(stringData)
+        if (noAuthCode.includes(parseJson.code)) {
+          noAuthDill()
+          return
+        } else {
+          return res
+        }
+      })
     }
     const { code, msg } = res.data
     const successCode = '0,200,20000'
-    const noAuthCode = '401,403'
     if (successCode.includes(code)) {
       return res.data
     } else {
-      if (noAuthCode.includes(code)) {
-        ElMessageBox.confirm('请重新登录', {
-          confirmButtonText: '重新登录',
-          closeOnClickModal: false,
-          showCancelButton: false,
-          showClose: false,
-          type: 'warning'
-        }).then(() => {
-          useBasicStore().resetStateAndToLogin()
-        })
+      //authorTipDoor 防止多个请求 多次alter
+      if (authorTipDoor) {
+        if (noAuthCode.includes(code)) {
+          noAuthDill()
+        } else {
+          // @ts-ignore
+          if (!res.config?.isNotTipErrorMsg) {
+            ElMessage.error({
+              message: msg,
+              duration: 2 * 1000
+            })
+          } else {
+            return res
+          }
+          return Promise.reject(msg)
+        }
       }
-      // @ts-ignore
-      if (!res.config?.isNotTipErrorMsg) {
-        ElMessage.error({
-          message: msg,
-          duration: 2 * 1000
-        })
-      }
-      return Promise.reject(msg)
     }
   },
   //响应报错
   (err) => {
-    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave)  //从请求集合中移除
+    //取消请求
+    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave)
     if (loadingInstance) {
       loadingInstance && loadingInstance.close()
     }
